99fbc05feb35d777a1f0f860bc4970b3645bbe8e
Vitor Hugo <vtr@hotmail.com.br>
2015-03-10 19:05:38 -0300

    'fixed little bugs in specific cases'

--- [2015-03-09] Challenge #205 [Easy] Friendly Date Ranges/[2015-03-09] Challenge #205 [Easy] Friendly Date Ranges.py
+++ [2015-03-09] Challenge #205 [Easy] Friendly Date Ranges/[2015-03-09] Challenge #205 [Easy] Friendly Date Ranges.py
@@ -1,6 +1,6 @@
  # [2015-03-09] Challenge #205 [Easy] Friendly Date Ranges
 
-ipt = '2015-04-01 2020-09-10 DMY'
+ipt = '2015-12-11 2016-12-11 DMY'
 month_dict = {
     '01': 'January',
     '02': 'February',
@@ -25,6 +25,7 @@ day_dict = {
     7: 'th',
     8: 'th',
     9: 'th',
+    0: 'th',
 }
 
 
@@ -32,15 +33,15 @@ def beautiful_dates(date, month_dict, day_dict):
     first_date, second_date, mode = [w1.split('-') for w1 in date.split()]
     month1, day1, year1 = fix_date(first_date, day_dict, month_dict)
     month2, day2, year2 = fix_date(second_date, day_dict, month_dict)
-    return '{} {}{} - {} {}{}'.format(month1, day1, ', '+year1 if year1 != year2 else '',
-        month2 if month2 != month1 else '', day2, ', '+year2 if year2 == year1 else '')
+    return '{} {}{} - {} {}{}{}'.format(month1, day1, ', ' + year1 if year1 != year2 else '',
+                                        month2 if month2 != month1 else '', day2, ', ' + year2 if year2 != year1 else '',
+                                         ', ' + year2 if year2 == year1 else '')
 
 
-
-def fix_date(date, day_dict, month_dict):    
+def fix_date(date, day_dict, month_dict):
     year = date[0]
     month = date[1]
     day = int(date[2])
-    return [month_dict[month], '{}{}'.format(day, day_dict[day] if day <= 4 else day_dict[day%9]), year]
+    return [month_dict[month], '{}{}'.format(day, day_dict[day] if day <= 4 else day_dict[day % 10]), year]
 
 print beautiful_dates(ipt, month_dict, day_dict)
