51c7e8c65a06fd8241a11080825cc63b272c181e
Vitor Hugo <vtr@hotmail.com.br>
2015-03-17 23:09:20 -0300

    'using Paul McGuires code'

--- [2015-03-11] Challenge #205 [Intermediate] RPN/[2015-03-11] Challenge #205 [Intermediate] RPN.py
+++ [2015-03-11] Challenge #205 [Intermediate] RPN/[2015-03-11] Challenge #205 [Intermediate] RPN.py
@@ -1,6 +1,8 @@
  # [2015-03-11] Challenge #205 [Intermediate] RPN
-import re
-from Queue import Queue as Q
+from pyparsing import Literal, CaselessLiteral, Word, Combine, Group, Optional,\
+    ZeroOrMore, Forward, nums, alphas
+import math
+import operator
 
 notations = '''      0+1
       20-18
@@ -17,14 +19,113 @@ notations = '''      0+1
 single_notations = [notation.replace(' ', '') for notation in notations.split('\n')]
 single_notations2 = [notation.replace('x', '*') for notation in single_notations]
 
+# Copyright 2003-2006 by Paul McGuire
+#
 
-def parser(notation):
-    _ = re.split("([)(+-/*])", notation.replace(" ", ""))
-    return [part for part in _ if part is not '']
-
-def isnumber(piece):
-    return True if piece not in '()\+-*' else False 
+exprStack = []
 
 
+def pushFirst(strg, loc, toks):
+    exprStack.append(toks[0])
 
-print isnumber('*')
+
+def pushUMinus(strg, loc, toks):
+    if toks and toks[0] == '-':
+        exprStack.append('unary -')
+        #~ exprStack.append( '-1' )
+        #~ exprStack.append( '*' )
+
+bnf = None
+
+
+def BNF():
+    """
+    expop   :: '^'
+    multop  :: '*' | '/'
+    addop   :: '+' | '-'
+    integer :: ['+' | '-'] '0'..'9'+
+    atom    :: PI | E | real | fn '(' expr ')' | '(' expr ')'
+    factor  :: atom [ expop factor ]*
+    term    :: factor [ multop factor ]*
+    expr    :: term [ addop term ]*
+    """
+    global bnf
+    if not bnf:
+        point = Literal(".")
+        e = CaselessLiteral("E")
+        fnumber = Combine(Word("+-" + nums, nums) +
+                          Optional(point + Optional(Word(nums))) +
+                          Optional(e + Word("+-" + nums, nums)))
+        ident = Word(alphas, alphas + nums + "_$")
+
+        plus = Literal("+")
+        minus = Literal("-")
+        mult = Literal("*")
+        div = Literal("/")
+        lpar = Literal("(").suppress()
+        rpar = Literal(")").suppress()
+        addop = plus | minus
+        multop = mult | div
+        expop = Literal("^")
+        pi = CaselessLiteral("PI")
+
+        expr = Forward()
+        atom = (Optional("-") + (pi | e | fnumber | ident + lpar + expr + rpar).setParseAction(
+            pushFirst) | (lpar + expr.suppress() + rpar)).setParseAction(pushUMinus)
+
+        # by defining exponentiation as "atom [ ^ factor ]..." instead of "atom [ ^ atom ]...", we get right-to-left exponents, instead of left-to-righ
+        # that is, 2^3^2 = 2^(3^2), not (2^3)^2.
+        factor = Forward()
+        factor << atom + ZeroOrMore((expop + factor).setParseAction(pushFirst))
+
+        term = factor + ZeroOrMore((multop + factor).setParseAction(pushFirst))
+        expr << term + ZeroOrMore((addop + term).setParseAction(pushFirst))
+        bnf = expr
+    return bnf
+
+# map operator symbols to corresponding arithmetic operations
+epsilon = 1e-12
+opn = {"+": operator.add,
+       "-": operator.sub,
+       "*": operator.mul,
+       "/": operator.truediv,
+       "^": operator.pow}
+fn = {"sin": math.sin,
+      "cos": math.cos,
+      "tan": math.tan,
+      "abs": abs,
+      "trunc": lambda a: int(a),
+      "round": round,
+      "sgn": lambda a: abs(a) > epsilon and cmp(a, 0) or 0}
+
+
+def evaluateStack(s):
+    op = s.pop()
+    if op == 'unary -':
+        return -evaluateStack(s)
+    if op in "+-*/^":
+        op2 = evaluateStack(s)
+        op1 = evaluateStack(s)
+        return opn[op](op1, op2)
+    elif op == "PI":
+        return math.pi  # 3.1415926535
+    elif op == "E":
+        return math.e  # 2.718281828
+    elif op in fn:
+        return fn[op](evaluateStack(s))
+    elif op[0].isalpha():
+        return 0
+    else:
+        return float(op)
+
+if __name__ == "__main__":
+
+    def test(s):
+        global exprStack
+        exprStack = []
+        results = BNF().parseString(s)
+        val = evaluateStack(exprStack[:])
+        print s + '=' + str(val), results, "=>", exprStack
+
+    for notation in single_notations2:
+        test(notation)
