8d238e9f76800e1cfb95d00aed9372069dd6a84e
Vitor Hugo <vtr@hotmail.com.br>
2015-03-19 17:25:35 -0300

    fixed some inputs

--- [2015-03-16] Challenge #206 [Easy] Recurrence Relations, part 1/[2015-03-16] Challenge #206 [Easy] Recurrence Relations, part 1.py
+++ [2015-03-16] Challenge #206 [Easy] Recurrence Relations, part 1/[2015-03-16] Challenge #206 [Easy] Recurrence Relations, part 1.py
@@ -1,9 +1,11 @@
  #[2015-03-16] Challenge #206 [Easy] Recurrence Relations, part 1
-ipt = '*2)+1'
+ipt = '*2 +1'
 
 def recursive(n, expression):
-    result = '({}{}'.format(n, expression)
-    print result
+    terms = expression.split()
+    terms[0] = terms[0] + ')'
+    terms = ' '.join(terms)
+    result = '({}{}'.format(n, terms)
     return eval(result)
 
 
@@ -12,4 +14,4 @@ def call_many_times(how_many, expression, first_term = 0):
    return first_term if how_many == 0 else call_many_times(how_many-1, expression, recursive(first_term, ipt)) 
 
 
-print call_many_times(3, ipt)
+print call_many_times(10, ipt, 0)
